d5c5c4970107471894228d1ff095bf7444a2fff5
Francis Indaheng <findaheng@berkeley.edu>
2020-03-22 18:38:20 -0700

    login complete

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # linkedin-bot
 Python script to automate the process of accepting LinkedIn connection invitations.
+
+Dependencies:
+1. Download chromedriver (https://chromedriver.chromium.org/)
+2. Install Selenium (pip install selenium) in Python virtual environment
--- bot.py
+++ bot.py
@@ -1,5 +1,36 @@
 from selenium import webdriver
 
+import sys
+sys.path.insert(0, '../z-linkedin_info')
+from info import username, password
+
 class LinkedInBot():
 	def __init__(self):
-		
+		self.driver = webdriver.Chrome()
+		self.driver.get('https://www.linkedin.com/login')
+
+	def login(self):
+		email_in = self.driver.find_element_by_xpath('//*[@id="username"]')
+		email_in.send_keys(username)
+
+		pw_in = self.driver.find_element_by_xpath('//*[@id="password"]')
+		pw_in.send_keys(password)
+
+		# TODO: Consider FSM for confirm information page
+
+		# NOTE: LinkedIn appears to randomly switch between div[4] and div[3]
+		try:
+			login_btn = self.driver.find_element_by_xpath('//*[@id="app__container"]/main/div/form/div[4]/button')
+			login_btn.click()
+		except:
+			login_btn = self.driver.find_element_by_xpath('//*[@id="app__container"]/main/div/form/div[3]/button')
+			login_btn.click()
+
+	def accept_invitations(self):
+		self.driver.get('https://www.linkedin.com/mynetwork/invitation-manager/')
+
+		#TODO
+
+if __name__ == '__main__':
+	bot = LinkedInBot()
+	bot.login()
